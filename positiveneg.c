b6d839b860f356f3f71750c5a6c45030fb0ebd7b
sdeepica <sdeeps1028@gmail.com>
2016-12-21 11:09:17 +0530

    Add files via upload

---
+++ positiveneg.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the no.");
+scanf("%d",&n);
+if(n>0)
+printf("the no. is positive");
+if(n<0)
+printf("the no is negative");
+if(n==0)
+printf("the number is zero");
+getch();
+}
