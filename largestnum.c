4ffe12501c6dd54eb6145b16f7bcf5883f4f26cb
sdeepica <sdeeps1028@gmail.com>
2016-12-21 11:22:08 +0530

    Add files via upload

---
+++ largestnum.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+a=8;
+b=6;
+c=44;
+if(a>b)  if(a>c)
+{
+printf("a is greater");
+getc(a);
+}
+else{
+    printf("c is greater");
+           getc(c);
+           }
+           else if(b>c)
+           {
+               printf("b is greater");
+               getc(b);
+           }
+               else{
+                printf("c is greater");
+                       getc(c);
+                       }
+               }
+
+
